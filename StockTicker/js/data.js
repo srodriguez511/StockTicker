1820a5cf8e55fba3fa4a1ac183316ded9750b9f9
Steven Rodriguez <srodriguez511@gmail.com>
2013-05-16 18:57:12 -0400

    Initial

---
+++ StockTicker/js/data.js
@@ -0,0 +1,77 @@
+﻿/// <reference path="../Scripts/knockout-2.2.1.debug.js" />
+/// <reference path="../Scripts/rx.jquery.js" />
+/// <reference path="../Scripts/rx.js" />
+/// <reference path="../Scripts/rx.aggregates.js" />
+/// <reference path="../Scripts/rx.jquery.js" />
+/// <reference path="../Scripts/rx.time.js" />
+
+
+function GetStockTicks(numTicks) {
+    //Create fake stock prices
+    function generateStockPrice() {
+        return Math.random().toFixed(3);
+    }
+
+    //Create fake stock tickers
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (var i = 0; i < 3; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+   
+    //Generate random stock prices forever
+    var obs = Rx.Observable.generateWithRelativeTime(
+	    0, 				// Initial State
+	    function (x) {  // Condition
+	        return true;
+	    },
+	    function (x) {	// Iterate.. Don't really care about this
+	        return x + 1;
+	    },
+	    function (x) {	// Result Selector - Generate a new stock price
+	        return generateStockPrice();
+	    },
+        function (x) {
+            return 1000; /*ms*/
+        });
+
+    //Generate a random initial stock tick
+    function GenerateStockTick() {
+        function StockTick(Tick, Price) {
+            this.Tick = ko.observable(Tick);
+            this.Price = ko.observable(Price);
+            this.Change = ko.observable(0);
+        };
+        
+        //Create a new stock with an initial price
+        var stock = new StockTick(makeid(), generateStockPrice());
+
+        //Subscribe to the stock generator
+        obs.subscribe(function (n) {
+            var temp = (n - stock.Price()).toFixed(3);
+            stock.Price(n);
+            stock.Change(temp);
+        });
+
+        return stock;
+    };
+
+    //Array of stocks
+    var stocks = new Array();
+    for (var i = 0; i < numTicks; i++) {
+        stocks[i] = GenerateStockTick();
+    }
+
+    return stocks;
+};
+
+
+
+
+
+
+
---
+++ StockTicker/js/main.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="../Scripts/jquery-2.0.0-vsdoc.js" />
+/// <reference path="../Scripts/knockout-2.2.1.js" />
+/// <reference path="data.js" />
+/// <reference path="../Scripts/rx.js" />
+/// <reference path="../Scripts/rx.aggregates.js" />
+/// <reference path="../Scripts/rx.jquery.js" />
+/// <reference path="../Scripts/rx.time.js" />
+
+
+$(document).ready(function () {
+    var numStocks = 4;
+    var comp1 = new GetStockTicks(numStocks);
+    var changes = new Array(numStocks);
+
+    //Setup the VM
+    function MyViewModel() {
+        var self = this;
+        self.stocks = ko.observableArray(comp1)
+        self.changes = ko.observableArray(changes);
+    }
+
+    //must apply this before we grab the table rows 
+    ko.applyBindings(new MyViewModel());
+
+    //Row 0 is the header ignore this
+    var rows = $('#stockTable tr');
+    $.each(comp1, function (index) {
+
+        this.Change.subscribe(function (value) {
+            //add 1  to the index to offset the header row
+            //children[2] addresses the change column
+            
+            var thisRow = rows[index + 1];
+            var thisCol = thisRow.children[2];
+
+            //Check the content of this array item and 
+            //determine what to color it
+            if (thisCol.textContent >= 0) {
+                thisCol.bgColor = "green";
+            }
+            else {
+                thisCol.bgColor = "red";
+            }
+
+        });
+    });
+
+
+});
+
